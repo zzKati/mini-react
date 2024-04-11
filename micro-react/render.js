01b41122e9e52c43315a97c094ac9d9de6b4d8b0
apple <apple@appledeMacBook-Pro.local>
2024-04-11 21:10:32 +0800

    finish concurrent mode

--- micro-react/render.js
+++ micro-react/render.js
@@ -7,12 +7,50 @@ function render(element, container) {
         dom[key] = element.props[key]
     })
 
-    // 递归渲染children节点
-    element.props.children.forEach(child => {
-        render(child, dom)
-    })
+    // // 递归渲染children节点
+    // element.props.children.forEach(child => {
+    //     render(child, dom)
+    // })
+    /**
+     * 上方的递归处理是不好的 在React中采用了一种名为 Fiber的新调度方法
+     * 它会在合适的时刻渲染节点
+     * 而不是递归渲染节点 因为递归是无法停止的
+     * 此处的核心是requestIdleCallback()
+     * 它会在浏览器空闲时执行回调函数
+     * 但react官方不再使用这个API 转而使用scheduler package
+     */
 
     container.appendChild(dom)
+}
+
+// 下次渲染工作 在render中初始化
+let nextUnitOfWork = null
+
+/**
+ * // 调度工作函数
+ * @param {*} deadLine 剩余可执行时间
+ */
+function workLoop(deadLine) {
+    // 判断是否应该停止工作
+    let shouldYield = false
+
+    while (nextUnitOfWork && !shouldYield) {
+        // 处理当前任务并获得下次任务
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+
+        // 检查是否还有时间 小于1ms停止
+        shouldYield = deadLine.timeRemaining() < 1
+    }
+
+    // 等待浏览器下次空闲时调用
+    requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+// 执行工作函数
+function performUnitOfWork() {
+
 }
 
 
